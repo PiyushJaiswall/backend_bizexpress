fd1a7b917914f0e80b6d74cb86f5deef607233d2
Piyush jaiswal <92733674+PiyushJaiswall@users.noreply.github.com>
2025-09-23 16:26:55 +0530

    Delete app/summarization.py

--- app/summarization.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def summarize_text(text: str):
-    return summarizer(text, max_length=150, min_length=50, do_sample=False)[0]['summary_text']
